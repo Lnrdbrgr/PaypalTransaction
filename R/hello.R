a17c597ce40597bde9d39be3e2be7fd0a916eded
Leonard Berger <leonard.berger@student.uni-tuebingen.de>
2022-08-29 10:08:59 +1000

    First Commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+#' @title a title
+#'
+#' @description a description
+#'
+#' @param x The parameter
+#' @param y The second parameter
+#'
+#' @return The output
+#' @export
+#'
+#' @examples
+#' hello("John")
+hello <- function(x, y) {
+  print(paste0("Hello, world! ", x, y))
+}
